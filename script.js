cea78a5de08dc43b37f260e818e3a084f072b8d2
Tomas <nexionly@gmail.com>
2023-07-26 16:53:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function calculateSeverity() {
+    const functionalityScore = parseFloat(document.getElementById("functionality").value);
+    const frequencyScore = parseFloat(document.getElementById("frequency").value);
+    const workaroundsScore = parseFloat(document.getElementById("workarounds").value);
+    const affectedUsersScore = parseFloat(document.getElementById("affectedUsers").value);
+  
+    const medianScore = (functionalityScore + frequencyScore + workaroundsScore + affectedUsersScore) / 4;
+    const severityLevel = getSeverityLevel(medianScore);
+  
+    document.getElementById("medianScore").textContent = medianScore.toFixed(4);
+    document.getElementById("severityLevel").textContent = severityLevel;
+    updateSeverityColor(severityLevel);
+  }
+  
+  function getSeverityLevel(score) {
+    if (score >= 3.6666) {
+      return "High";
+    } else if (score >= 2.3333) {
+      return "Medium";
+    } else {
+      return "Low";
+    }
+  }
+  
+  function updateSeverityColor(severityLevel) {
+    const severityElement = document.getElementById("severityLevel");
+    severityElement.classList.remove("severity-low", "severity-medium", "severity-high");
+    if (severityLevel === "High") {
+      severityElement.classList.add("severity-high");
+    } else if (severityLevel === "Medium") {
+      severityElement.classList.add("severity-medium");
+    } else {
+      severityElement.classList.add("severity-low");
+    }
+  }
+  
